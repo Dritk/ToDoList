f65ebed3d112471e6690004d4b407be7aea274cc
Dritk <dritkarmacharya@gmail.com>
2025-02-04 16:46:15 +0545

    change

--- src/assets/Components/TodoApp.tsx
+++ src/assets/Components/TodoApp.tsx
@@ -5,13 +5,21 @@ export interface TodoI {
   title: string;
   id: number;
 }
+
 const TodoApp = () => {
   const [todo, setTodo] = useState<TodoI>({ title: "", id: 0 });
   const [todos, setTodos] = useState<TodoI[]>([]);
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTodo({ title: e.target.value, id: 0 });
+    setTodo({ title: e.target.value, id: todo.id });
   };
-  const onAdd = (e: React.MouseEvent) => {
+
+  const onAdd = () => {
+    if (todo.title.trim() === "") {
+      //trim is a function used to remove whitespaces i.e. doesnt allow array to be filled empty
+      alert("Enter something");
+      return;
+    }
     const id = Math.floor(Math.random() * 100);
     setTodos([...todos, { title: todo.title, id }]);
     setTodo({ title: "", id: 0 });
@@ -21,14 +29,16 @@ const TodoApp = () => {
     setTodos([]);
     setTodo({ title: "", id: 0 });
   };
+
   return (
     <div>
       <h1>ToDoList</h1>
       <input
         type="Text"
         placeholder="Enter your To Do List"
+        value={todo.title}
         onChange={onChange}
-      ></input>
+      />
       <button className="btn1" onClick={onAdd}>
         Add
       </button>
